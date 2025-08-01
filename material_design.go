217357209401f81e40c63464d11da1d9f445c2bb
Richard Knowler <richardknowler@iMac.local>
2025-08-02 11:34:43 +1200

    added material design components

---
+++ material_design.go
@@ -0,0 +1,219 @@
+// Package htma provides a Hypertext Markup Abstraction for generating HTML in pure Go.
+package htma
+
+// Material Design Components
+
+// MdFilledButton creates a new md-filled-button element.
+func MdFilledButton() Element {
+	return newElement("md-filled-button", false)
+}
+
+// MdOutlinedButton creates a new md-outlined-button element.
+func MdOutlinedButton() Element {
+	return newElement("md-outlined-button", false)
+}
+
+// MdTextButton creates a new md-text-button element.
+func MdTextButton() Element {
+	return newElement("md-text-button", false)
+}
+
+// MdElevatedButton creates a new md-elevated-button element.
+func MdElevatedButton() Element {
+	return newElement("md-elevated-button", false)
+}
+
+// MdTonalButton creates a new md-tonal-button element.
+func MdTonalButton() Element {
+	return newElement("md-tonal-button", false)
+}
+
+// MdFab creates a new md-fab element.
+func MdFab() Element {
+	return newElement("md-fab", false)
+}
+
+// MdIconButton creates a new md-icon-button element.
+func MdIconButton() Element {
+	return newElement("md-icon-button", false)
+}
+
+// MdFilledIconButton creates a new md-filled-icon-button element.
+func MdFilledIconButton() Element {
+	return newElement("md-filled-icon-button", false)
+}
+
+// MdOutlinedIconButton creates a new md-outlined-icon-button element.
+func MdOutlinedIconButton() Element {
+	return newElement("md-outlined-icon-button", false)
+}
+
+// MdTonalIconButton creates a new md-tonal-icon-button element.
+func MdTonalIconButton() Element {
+	return newElement("md-tonal-icon-button", false)
+}
+
+// MdDialog creates a new md-dialog element.
+func MdDialog() Element {
+	return newElement("md-dialog", false)
+}
+
+// MdLinearProgress creates a new md-linear-progress element.
+func MdLinearProgress() Element {
+	return newElement("md-linear-progress", false)
+}
+
+// MdCircularProgress creates a new md-circular-progress element.
+func MdCircularProgress() Element {
+	return newElement("md-circular-progress", false)
+}
+
+// MdSnackbar creates a new md-snackbar element.
+func MdSnackbar() Element {
+	return newElement("md-snackbar", false)
+}
+
+// MdElevatedCard creates a new md-elevated-card element.
+func MdElevatedCard() Element {
+	return newElement("md-elevated-card", false)
+}
+
+// MdFilledCard creates a new md-filled-card element.
+func MdFilledCard() Element {
+	return newElement("md-filled-card", false)
+}
+
+// MdOutlinedCard creates a new md-outlined-card element.
+func MdOutlinedCard() Element {
+	return newElement("md-outlined-card", false)
+}
+
+// MdDivider creates a new md-divider element.
+func MdDivider() Element {
+	return newElement("md-divider", false)
+}
+
+// MdList creates a new md-list element.
+func MdList() Element {
+	return newElement("md-list", false)
+}
+
+// MdListItem creates a new md-list-item element.
+func MdListItem() Element {
+	return newElement("md-list-item", false)
+}
+
+// MdMenu creates a new md-menu element.
+func MdMenu() Element {
+	return newElement("md-menu", false)
+}
+
+// MdMenuItem creates a new md-menu-item element.
+func MdMenuItem() Element {
+	return newElement("md-menu-item", false)
+}
+
+// MdSubMenu creates a new md-sub-menu element.
+func MdSubMenu() Element {
+	return newElement("md-sub-menu", false)
+}
+
+// MdTabs creates a new md-tabs element.
+func MdTabs() Element {
+	return newElement("md-tabs", false)
+}
+
+// MdPrimaryTab creates a new md-primary-tab element.
+func MdPrimaryTab() Element {
+	return newElement("md-primary-tab", false)
+}
+
+// MdSecondaryTab creates a new md-secondary-tab element.
+func MdSecondaryTab() Element {
+	return newElement("md-secondary-tab", false)
+}
+
+// MdCheckbox creates a new md-checkbox element.
+func MdCheckbox() Element {
+	return newElement("md-checkbox", false)
+}
+
+// MdChipSet creates a new md-chip-set element.
+func MdChipSet() Element {
+	return newElement("md-chip-set", false)
+}
+
+// MdAssistChip creates a new md-assist-chip element.
+func MdAssistChip() Element {
+	return newElement("md-assist-chip", false)
+}
+
+// MdFilterChip creates a new md-filter-chip element.
+func MdFilterChip() Element {
+	return newElement("md-filter-chip", false)
+}
+
+// MdInputChip creates a new md-input-chip element.
+func MdInputChip() Element {
+	return newElement("md-input-chip", false)
+}
+
+// MdSuggestionChip creates a new md-suggestion-chip element.
+func MdSuggestionChip() Element {
+	return newElement("md-suggestion-chip", false)
+}
+
+// MdRadio creates a new md-radio element.
+func MdRadio() Element {
+	return newElement("md-radio", false)
+}
+
+// MdFilledSelect creates a new md-filled-select element.
+func MdFilledSelect() Element {
+	return newElement("md-filled-select", false)
+}
+
+// MdOutlinedSelect creates a new md-outlined-select element.
+func MdOutlinedSelect() Element {
+	return newElement("md-outlined-select", false)
+}
+
+// MdSlider creates a new md-slider element.
+func MdSlider() Element {
+	return newElement("md-slider", false)
+}
+
+// MdSwitch creates a new md-switch element.
+func MdSwitch() Element {
+	return newElement("md-switch", false)
+}
+
+// MdFilledTextField creates a new md-filled-text-field element.
+func MdFilledTextField() Element {
+	return newElement("md-filled-text-field", false)
+}
+
+// MdOutlinedTextField creates a new md-outlined-text-field element.
+func MdOutlinedTextField() Element {
+	return newElement("md-outlined-text-field", false)
+}
+
+// MdElevation creates a new md-elevation element.
+func MdElevation() Element {
+	return newElement("md-elevation", false)
+}
+
+// MdFocusRing creates a new md-focus-ring element.
+func MdFocusRing() Element {
+	return newElement("md-focus-ring", false)
+}
+
+// MdIcon creates a new md-icon element.
+func MdIcon() Element {
+	return newElement("md-icon", false)
+}
+
+// MdRipple creates a new md-ripple element.
+func MdRipple() Element {
+	return newElement("md-ripple", false)
+}
